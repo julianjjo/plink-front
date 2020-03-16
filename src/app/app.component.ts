fc96bc7cac1ee38841020c990c2be3bd3d9fb54a
Julian Mican <julian.mican@gmail.com>
2020-03-16 15:51:28 -0500

    Terminado footer con imagen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-// TODO: Terminar de organizar el footer
 export class AppComponent {
   title = 'Exchange Cryptocurrencies';
   selectedMenuItem = 'REALIZAR CAMBIO';
